db58da58100581a8521f32543658d4250ed301e9
Eric Kotzman <eric_kotzman1@my.vcccd.edu>
2026-04-13 00:54:15 +0000

    Jira integration testing

--- Combo_Scanner_Root/process_lib/src/lib.rs
+++ Combo_Scanner_Root/process_lib/src/lib.rs
@@ -5,9 +5,21 @@ pub struct ProcessInfo {
     pub rss_kb: i32,
 }
 
+#[repr(C)] // create a policy struct in C format
+pub struct Policy {
+    pub enfore_non_negative_rss: bool,
+    pub normalize_uid: bool,
+}
+
 #[no_mangle]
 //extern C dictates the calling convention or ABI for this function, which in this case is C.
-pub extern "C" fn get_processes(buffer: *mut ProcessInfo, count: i32) {
+pub extern "C" fn get_processes(
+    buffer: *mut ProcessInfo,
+    count: i32,
+    policy: Policy,
+) 
+
+{
     let slice = unsafe {
         std::slice::from_raw_parts_mut(buffer, count as usize)
 
@@ -15,12 +27,16 @@ pub extern "C" fn get_processes(buffer: *mut ProcessInfo, count: i32) {
 
     for i in 0..slice.len() {
         slice[i].pid = 2000 + i as i32;
-        slice[i].uid = if i % 2 == 0 {0} else {1000};
+        let mut uid = if i % 2 == 0 {0} else {500};
         
         let mut rss = (i as i32 + 1) * 1024; //create a mutable var to use for validation logic
-        if rss < 0 {
+        if policy.enfore_non_negative_rss && rss < 0 {
             rss = 0;
         }
-        slice[i].rss_kb = rss // assign validated var back to our struct value
+        if policy.normalize_uid && uid != 0 && uid < 1000 {
+            uid = 1000;
+        }
+        slice[i].uid = uid;
+        slice[i].rss_kb = rss; // assign validated var back to our struct value
     }
 }
